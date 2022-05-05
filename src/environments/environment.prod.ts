7f9cd71f70391bafa2d242058d69c0091be851eb
Paweł Marcinkowski <you@example.com>
2022-05-05 22:56:33 +0200

    add firebase: initial setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'bazarek-4aa99',
+    appId: '1:263167886281:web:e8ac17d786be6862db9e18',
+    storageBucket: 'bazarek-4aa99.appspot.com',
+    apiKey: 'AIzaSyCEmmGAdLqhcZ45M8kQuuAUHnYJgIwVOt0',
+    authDomain: 'bazarek-4aa99.firebaseapp.com',
+    messagingSenderId: '263167886281',
+    measurementId: 'G-VC9THE1PCM',
+  },
   production: true
 };
